f5c7d20fcb57a71b416fa7d2cbefe34c9b3523da
Itisafeature <thebigdebugger@gmail.com>
2022-05-25 21:01:08 -0400

    added faker gem and updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Comment.destroy_all
+Idea.destroy_all
+
+10.times do 
+  Idea.create(title: Faker::Lorem.characters(number: 15, min_alpha: 15), content: Faker::Lorem.paragraph(random_sentences_to_add: 4))
+end
+
+20.times do
+  Comment.create(content: Faker::Lorem.paragraph, idea_id: Idea.all.sample.id)
+end
+
+
