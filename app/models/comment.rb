9cd4f7bcdd5729fac7524a7a016ef8299510fdd7
Itisafeature <thebigdebugger@gmail.com>
2022-05-25 20:51:55 -0400

    added relationships and validations to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :idea
+
+  validates :content, presence: { message: "must be a minimum of 10 characters long" }, length: { minimum: 10, message: "must be a minimum of 10 characters long" }
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,2 +1,5 @@
 class Idea < ApplicationRecord
+  has_many :comments
+  validates :title, presence: { message: "must be a minimum of 5 characters long" }, length: { minimum: 5, message: "must be a minimum of 5 characters long" }
+  validates :content, presence: { message: "must be a minimum of 10 characters long" }, length: { minimum: 10, message: "must be a minimum of 10 characters long" }
 end
