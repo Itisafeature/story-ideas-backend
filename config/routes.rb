8f3b6317fbbb346499555257a3208e151df48e16
Itisafeature <thebigdebugger@gmail.com>
2022-05-25 21:12:28 -0400

    namespaced ideas controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :ideas
+
+  namespace :api do 
+    namespace :v1 do 
+      resources :ideas, only: [:index]
+    end
+  end
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
