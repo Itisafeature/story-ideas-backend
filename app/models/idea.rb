ee0e3eae6c8bf4554814c3aa8c52482684ddff1b
Itisafeature <thebigdebugger@gmail.com>
2022-06-18 18:23:57 -0400

    added scope for recently commented ideas

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -4,4 +4,6 @@ class Idea < ApplicationRecord
   validates :content, presence: { message: "Content must be a minimum of 10 characters long" }, length: { minimum: 10, message: "Content must be a minimum of 10 characters long" }
 
   scope :order_by_created, -> { order(created_at: :desc) }
+  scope :most_recently_commented, -> { select('ideas.*, MAX(comments.created_at)').joins(:comments).order('com
+  ments.created_at desc').group('ideas.id').limit(9) }
 end
