5dc94e6abd717d1e4d14c21a990f8b50f131ccb6
Itisafeature <thebigdebugger@gmail.com>
2022-06-20 20:44:06 -0400

    fixed syntax error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
 
   validates :content, presence: { message: "must be a minimum of 10 characters long" }, length: { minimum: 10, message: "must be a minimum of 10 characters long" }
 
-  scope: :order_by_most_recent, -> {order('created_at desc')}
+  scope :order_by_most_recent, -> {order('created_at desc')}
 end
