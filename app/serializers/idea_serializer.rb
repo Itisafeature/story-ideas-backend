5fd29aa866a3d987da240c74cd18ebea1b99e50f
Itisafeature <thebigdebugger@gmail.com>
2022-06-23 14:47:53 -0400

    updated idea serializer to use scope for comments

--- app/serializers/idea_serializer.rb
+++ app/serializers/idea_serializer.rb
@@ -10,6 +10,6 @@ class IdeaSerializer
   attribute :content, if: Proc.new { |record, params| !params[:id].nil? }
 
   attribute :comments, if: Proc.new { |record, params| !params[:id].nil? } do |idea|
-    CommentSerializer.new(idea.comments)
+    CommentSerializer.new(idea.comments.order_by_most_recent)
   end 
 end
